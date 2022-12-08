3d4f06d8466d4abaee257405eb4471e573c34e8c
SaideepArikontham <104340044+SaideepArikontham@users.noreply.github.com>
2022-12-08 12:28:06 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,12 +1,2 @@
-# Databricks notebook source
-def demo1():
-    print("this is demo1 in check.py")
-
-# COMMAND ----------
-
-def demo2():
-    print("this is demo2 in check.py")
-
-# COMMAND ----------
-
-
+def n_to_mth(n,m):
+  print(n, "to the", m, "th power is", n**m)
