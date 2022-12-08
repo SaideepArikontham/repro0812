c193e72ab2a1e833574c2db6f3ebcd14b116f149
SaideepArikontham <104340044+SaideepArikontham@users.noreply.github.com>
2022-12-08 12:43:17 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+def demo1():
+    print("this is demo1 in check.py")
+
+# COMMAND ----------
+
+def demo2():
+    print("this is demo2 in check.py")
+
+# COMMAND ----------
+
+
